1ab98b34a0e9bf0db2c9a760c336dd7d8f85b6bb
Laynt <laynt00@gmail.com>
2024-07-26 22:27:01 +0200

    death trigger

--- Scripts/Spikes.gd
+++ Scripts/Spikes.gd
@@ -14,7 +14,8 @@ func _process(delta):
 
 func _on_area_2d_body_entered(body):
 	if body.name == "Player":
-		print("Me pinché")
 		player.currentHealth -= 1
 		print(player.currentHealth)
+	#if player.currentHealth < 0:
+		#llamar a la animación de muerte
 		
